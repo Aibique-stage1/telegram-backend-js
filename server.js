5e4558ef2f785d501e5bb3adc8f079b2e344308a
aybike <tonn.raus09@gmail.com>
2021-04-17 12:23:48 +0200

    refactor: app methods instead of router

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 const express = require('express')
 const PORT = 3000
-const router = express.Router();
 const app = express();
 
-app.use(router);
 
-router.get('/get', function(req, res){
+app.get('/get', function(req, res){
     res.json({
         "METHOD": "GET",
         "use": "It allows to get information",
         "example": "User info received 👀 "
     })
 })
-router.post('/post', function(req, res){
+app.post('/post', function(req, res){
     res.json({
         "METHOD": "POST",
         "use": "It creates a new Object in the server",
         "example":"Add a image to the carrousel 👍"
     })
 })
-router.put('/put', function(req, res){
+app.put('/put', function(req, res){
     res.json({
         "METHOD": "PUT",
         "use": "when big changes on an object are necessary",
         "example": "Edit the user Profile"
     })
 })
-router.delete('/delete', function(req,res){
+app.delete('/delete', function(req,res){
     res.json({
         "METHOD": "DELETE",
         "use": "It deletes the requested object",
